6e43aebd5c5a4af41b50942ffb26332fc24e266e
Tamir Feldman <tamir@shieldfc.com>
2022-01-30 18:49:40 +0200

    Updated project to Typescript + added keywords

--- src/landing/Landing.tsx
+++ src/landing/Landing.tsx
@@ -2,12 +2,34 @@ import React from 'react';
 import './Landing.scss'
 import { Button, Form } from 'react-bootstrap';
 
+interface LandingState {
+    question: string;
+    keywords: string[];
+}
+
 export default class Landing extends React.Component {
-    state = {
+    state: LandingState = {
         question: "",
+        keywords: [],
     } 
 
+    componentDidMount() {
+        const keywords = this.getKeyWords();
+        this.setState({
+            keywords
+        })
+    }
+
+    getKeyWords(): string[] { //TODO: update to HTTP call
+        return [
+            "nausea",
+            "constipation",
+            "high blood pressure"
+        ]
+    }
+
     findAnswer() {
+        //TODO: handle question and keywords here
         console.log(this.state.question);
     }
 
@@ -21,7 +43,7 @@ export default class Landing extends React.Component {
                             type="text"
                             placeholder="Your question"
                             as="textarea"
-                            rows="3"
+                            rows={3}
                             value={this.state.question}
                             onChange={e => this.setState({question: e.target.value})} />
                     </Form.Group>
